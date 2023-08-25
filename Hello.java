83c68d6e304f2996fc4420b9e477c72d27831b98
JIN MORI <ramosjohnmichael61@gmail.com>
2023-08-25 23:47:45 +0800

    Adding new line to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is RAMOS, John Michael!");
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
   } 
 } 
